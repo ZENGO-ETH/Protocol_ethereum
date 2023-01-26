3be8fece563edd4ac3a66d00c1d90eac47830c1f
tommy4241 <54299064+tommy4241@users.noreply.github.com>
2023-01-27 01:38:09 +1000

    fix 8 (#99)
    Co-authored-by: woofabbit <woofabbit@woofabbits-iMac-Pro.local>

---
+++ test/Haechi/8.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+
+import "../../src/AssimilatorFactory.sol";
+import "../../src/CurveFactoryV2.sol";
+import "../../src/Curve.sol";
+import "../../src/interfaces/IERC20Detailed.sol";
+
+import ".././lib/MockUser.sol";
+import ".././lib/CheatCodes.sol";
+import ".././lib/Address.sol";
+import ".././lib/CurveParams.sol";
+
+contract FactoryAddressCheck is Test {
+    AssimilatorFactory assimilatorFactory;
+
+    function setUp() public {
+        assimilatorFactory = new AssimilatorFactory();
+    }
+
+    function testFailZeroFactoryAddress() public {
+        assimilatorFactory.setCurveFactory(address(0));
+        fail("AssimFactory/curve factory zero address!");
+    }
+}
