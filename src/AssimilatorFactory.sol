2979a5b6296294f9eab87472ec8f9d9652a4ce61
Harry Chan <chanharryhk@gmail.com>
2023-01-30 13:01:25 -0500

    Revert "fix 8 (#99)" (#117)
    This reverts commit 3be8fece563edd4ac3a66d00c1d90eac47830c1f.

--- src/AssimilatorFactory.sol
+++ src/AssimilatorFactory.sol
@@ -7,46 +7,24 @@ import "./interfaces/IAssimilatorFactory.sol";
 import "./interfaces/IOracle.sol";
 
 contract AssimilatorFactory is IAssimilatorFactory, Ownable {
-    event NewAssimilator(
-        address indexed caller,
-        bytes32 indexed id,
-        address indexed assimilator,
-        address oracle,
-        address token
-    );
-    event AssimilatorRevoked(
-        address indexed caller,
-        bytes32 indexed id,
-        address indexed assimilator
-    );
-    event CurveFactoryUpdated(
-        address indexed caller,
-        address indexed curveFactory
-    );
+    event NewAssimilator(address indexed caller, bytes32 indexed id, address indexed assimilator, address oracle, address token);
+    event AssimilatorRevoked(address indexed caller, bytes32 indexed id, address indexed assimilator);
+    event CurveFactoryUpdated(address indexed caller, address indexed curveFactory);
     mapping(bytes32 => AssimilatorV2) public assimilators;
 
     address public curveFactory;
 
-    modifier onlyCurveFactory() {
+    modifier onlyCurveFactory {
         require(msg.sender == curveFactory, "unauthorized");
         _;
     }
 
     function setCurveFactory(address _curveFactory) external onlyOwner {
-        require(
-            _curveFactory != address(0),
-            "AssimFactory/curve factory zero address!"
-        );
         curveFactory = _curveFactory;
         emit CurveFactoryUpdated(msg.sender, curveFactory);
     }
 
-    function getAssimilator(address _token)
-        external
-        view
-        override
-        returns (AssimilatorV2)
-    {
+    function getAssimilator(address _token) external view override returns (AssimilatorV2) {
         bytes32 assimilatorID = keccak256(abi.encode(_token));
         return assimilators[assimilatorID];
     }
@@ -59,20 +37,9 @@ contract AssimilatorFactory is IAssimilatorFactory, Ownable {
         bytes32 assimilatorID = keccak256(abi.encode(_token));
         if (address(assimilators[assimilatorID]) != address(0))
             revert("AssimilatorFactory/oracle-stablecoin-pair-already-exists");
-        AssimilatorV2 assimilator = new AssimilatorV2(
-            _oracle,
-            _token,
-            _tokenDecimals,
-            IOracle(_oracle).decimals()
-        );
+        AssimilatorV2 assimilator = new AssimilatorV2(_oracle, _token, _tokenDecimals, IOracle(_oracle).decimals());
         assimilators[assimilatorID] = assimilator;
-        emit NewAssimilator(
-            msg.sender,
-            assimilatorID,
-            address(assimilator),
-            address(_oracle),
-            _token
-        );
+        emit NewAssimilator(msg.sender, assimilatorID, address(assimilator), address(_oracle), _token);
         return assimilator;
     }
 
@@ -80,10 +47,6 @@ contract AssimilatorFactory is IAssimilatorFactory, Ownable {
         bytes32 assimilatorID = keccak256(abi.encode(_token));
         address _assimAddress = address(assimilators[assimilatorID]);
         assimilators[assimilatorID] = AssimilatorV2(address(0));
-        emit AssimilatorRevoked(
-            msg.sender,
-            assimilatorID,
-            address(_assimAddress)
-        );
+        emit AssimilatorRevoked(msg.sender, assimilatorID, address(_assimAddress));
     }
 }
